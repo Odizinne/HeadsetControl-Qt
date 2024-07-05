8a3f32025bac64a5b3888429b1deebb6e5639ba1
Odizinne <flora.aubry@proton.me>
2024-07-05 12:28:32 +0200

    use python design

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 import sys
 from cx_Freeze import setup, Executable
-# PyQt6 import statement for proper freezing
 
 base = None
+build_dir = "build/HeadsetControl-Qt"
 
 if sys.platform == "win32":
     base = "Win32GUI"
-    include_files = ["icons/", "dependencies/", "design.ui"]
-    zip_include_packages = ["PyQt6"]
-    if "winshell" in sys.modules:
-        zip_include_packages.append("winshell")
+    include_files = ["icons/", "dependencies/"]
+    zip_include_packages = ["PyQt6", "winshell"]
 elif sys.platform == "linux":
-    include_files = ["design.ui"]
+    include_files = []
     zip_include_packages = ["PyQt6"]
 
 build_exe_options = {
+    "build_exe": build_dir,
     "include_files": include_files,
     "zip_include_packages": zip_include_packages,
     "excludes": [],
