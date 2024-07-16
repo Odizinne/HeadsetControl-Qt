3b32bf0a65594ed199bffcf0bcc310a9936d9959
Odizinne <flora.aubry@proton.me>
2024-07-16 09:51:13 +0200

    Added install command

--- src/setup.py
+++ src/setup.py
@@ -1,8 +1,10 @@
 import os
 from cx_Freeze import setup, Executable
+from setuptools.command.install import install as _install
 
 src_dir = os.path.dirname(os.path.abspath(__file__))
 build_dir = "build/HeadsetControl-Qt"
+install_dir = os.path.join(os.getenv('LOCALAPPDATA'), 'programs', 'HeadsetControl-Qt')
 
 include_files = [
     os.path.join(src_dir, "dependencies/"), 
@@ -27,9 +29,23 @@ executables = [
     )
 ]
 
+class InstallCommand(_install):
+    def run(self):
+        if not os.path.exists(install_dir):
+            os.makedirs(install_dir)
+        if not os.path.exists(build_dir):
+            print("##################################################")
+            print("# Nothing to install.                            #")
+            print("# Please build the project first.                #")
+            print("##################################################")
+            return
+        self.copy_tree(build_dir, install_dir)
+        print(f"Executable installed to {install_dir}")
+        
 setup(
     name="HeadsetControl-Qt",
     version="1.0",
     options={"build_exe": build_exe_options},
-    executables=executables
+    executables=executables,
+    cmdclass={'install': InstallCommand}
 )
