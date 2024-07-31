3d39caf0100c0c57a24277ba46b20290c9c153b1
Odizinne <flora.aubry@proton.me>
2024-07-31 23:52:58 +0200

    added option to send notification on selected battery threshold

--- src/setup.py
+++ src/setup.py
@@ -5,12 +5,9 @@ import winshell
 
 src_dir = os.path.dirname(os.path.abspath(__file__))
 build_dir = "build/HeadsetControl-Qt"
-install_dir = os.path.join(os.getenv('LOCALAPPDATA'), 'programs', 'HeadsetControl-Qt')
+install_dir = os.path.join(os.getenv("LOCALAPPDATA"), "programs", "HeadsetControl-Qt")
 
-include_files = [
-    os.path.join(src_dir, "dependencies/"), 
-    os.path.join(src_dir, "icons")
-]
+include_files = [os.path.join(src_dir, "dependencies/"), os.path.join(src_dir, "icons")]
 
 zip_include_packages = ["PyQt6"]
 
@@ -23,10 +20,10 @@ build_exe_options = {
 
 executables = [
     Executable(
-        script = os.path.join(src_dir, 'headsetcontrol-qt.py'),
-        base = "Win32GUI",
-        icon = os.path.join(src_dir, "icons/icon.ico"),
-        target_name = "HeadsetControl-Qt",
+        script=os.path.join(src_dir, "headsetcontrol-qt.py"),
+        base="Win32GUI",
+        icon=os.path.join(src_dir, "icons/icon.ico"),
+        target_name="HeadsetControl-Qt",
     )
 ]
 
@@ -50,19 +47,16 @@ class InstallCommand(_install):
         icon = os.path.join(install_dir, "icons/icon.ico")
 
         winshell.CreateShortcut(
-            Path=shortcut_path,
-            Target=target,
-            Icon=(icon, 0),
-            Description="HeadsetControl-Qt",
-            StartIn=install_dir
+            Path=shortcut_path, Target=target, Icon=(icon, 0), Description="HeadsetControl-Qt", StartIn=install_dir
         )
 
         print("Created shortcut in start menu")
-        
+
+
 setup(
     name="HeadsetControl-Qt",
     version="1.0",
     options={"build_exe": build_exe_options},
     executables=executables,
-    cmdclass={'install': InstallCommand}
+    cmdclass={"install": InstallCommand},
 )
