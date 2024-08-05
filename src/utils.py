42da702391592bbfeb2b7982b11a34ffe3ead1e5
Odizinne <flora.aubry@proton.me>
2024-08-05 11:07:58 +0200

    fixed windows 10 detection

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,6 @@ import platform
 
 
 def is_windows_10():
-    os_version = platform.version()
+    os_release = platform.release()
     os_name = platform.system()
-
-    # Windows 10 versions are generally greater than 10.0.10240
-    # This can be fine-tuned as needed
-    print(f"{os_name}, {os_version}")
-    return os_name == "Windows" and os_version.startswith("10")
+    return os_name == "Windows" and os_release == "10"
