024f730037e0ab855ec4e7807a7507c305e24a8f
Odizinne <flora.aubry@proton.me>
2024-08-07 09:18:02 +0200

    minimize build output

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,5 @@
 import os
 from cx_Freeze import setup, Executable
-from setuptools.command.install import install as _install
-import winshell
 
 src_dir = os.path.dirname(os.path.abspath(__file__))
 build_dir = "build/HeadsetControl-Qt"
@@ -16,6 +14,7 @@ build_exe_options = {
     "include_files": include_files,
     "zip_include_packages": zip_include_packages,
     "excludes": ["tkinter"],
+    "silent": True,
 }
 
 executables = [
@@ -27,36 +26,9 @@ executables = [
     )
 ]
 
-
-class InstallCommand(_install):
-    def run(self):
-        if not os.path.exists(install_dir):
-            os.makedirs(install_dir)
-        if not os.path.exists(build_dir):
-            print("##################################################")
-            print("# Nothing to install.                            #")
-            print("# Please build the project first.                #")
-            print("##################################################")
-            return
-        self.copy_tree(build_dir, install_dir)
-        print("")
-        print(f"Executable installed to {install_dir}")
-
-        shortcut_path = os.path.join(winshell.programs(), "HeadsetControl-Qt.lnk")
-        target = os.path.join(install_dir, "HeadsetControl-Qt.exe")
-        icon = os.path.join(install_dir, "icons/icon.ico")
-
-        winshell.CreateShortcut(
-            Path=shortcut_path, Target=target, Icon=(icon, 0), Description="HeadsetControl-Qt", StartIn=install_dir
-        )
-
-        print("Created shortcut in start menu")
-
-
 setup(
     name="HeadsetControl-Qt",
     version="1.0",
     options={"build_exe": build_exe_options},
     executables=executables,
-    cmdclass={"install": InstallCommand},
 )
