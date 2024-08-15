877abf65ccf992578ab1ed2af350788131fe0e36
Odizinne <flora.aubry@proton.me>
2024-08-15 12:23:02 +0200

    fix theme index in create default settings

--- src/headsetcontrol-qt.py
+++ src/headsetcontrol-qt.py
@@ -131,13 +131,11 @@ class HeadsetControlApp(QMainWindow):
             "light_battery_threshold": 20,
             "notification_battery_threshold": 20,
             "sidetone": 0,
-            "theme": "System",
+            "theme": 0,
         }
         with open(SETTINGS_FILE, "w") as f:
             json.dump(settings, f, indent=4)
 
-
-    
     def update_headset_info(self):
         command = [HEADSETCONTROL_EXECUTABLE, "-o", "json"]
         creation_flags = subprocess.CREATE_NO_WINDOW if sys.platform == "win32" else 0
