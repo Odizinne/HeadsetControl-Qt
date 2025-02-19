f78dd46afdc20fba8804868109aa9fee50d971ca
DogancanYr <90002969+DogancanYr@users.noreply.github.com>
2025-02-19 21:05:00 +0300

    Turkish translation
    Added Turkish translation.

---
+++ Resources/tr/HeadsetControl-Qt_tr.ts
@@ -0,0 +1,207 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR">
+<context>
+    <name>HeadsetControlQt</name>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.ui" line="26"/>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.cpp" line="346"/>
+        <source>HeadsetControl-Qt</source>
+        <translation>Kulaklık Kontolcüsü</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.ui" line="188"/>
+        <source>Language</source>
+        <translation>Dil</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.ui" line="280"/>
+        <source>Battery</source>
+        <translation>Pil</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.ui" line="309"/>
+        <source>Sidetone</source>
+        <translation>Mikrafon dinleme sesi</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.ui" line="324"/>
+        <source>Lights</source>
+        <translation>Işık</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.ui" line="359"/>
+        <source>Disable lights on low battery</source>
+        <translation>Düşük pil seviyesinde ışıkları kapat</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.ui" line="388"/>
+        <source>Send notification on low battery</source>
+        <translation>Düşük pilde bildirim gönder</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.ui" line="417"/>
+        <source>Beep on low battery</source>
+        <translation>Düşük pilde biple</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.ui" line="237"/>
+        <source>Settings</source>
+        <translation>Ayarlar</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.ui" line="146"/>
+        <source>Icon theme</source>
+        <translation>İkon teması</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.ui" line="133"/>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.cpp" line="109"/>
+        <source>Run at startup</source>
+        <translation>Başlangıçta başlat</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.ui" line="82"/>
+        <source>Low battery threshold</source>
+        <translation>Düşük pil eşiği</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.ui" line="57"/>
+        <source>No supported headset found.</source>
+        <translation>Desteklenen kulaklık bulunamadı. :(</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.cpp" line="108"/>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.cpp" line="325"/>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.cpp" line="404"/>
+        <source>Show</source>
+        <translation>Göster</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.cpp" line="110"/>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.cpp" line="406"/>
+        <source>Exit</source>
+        <translation>Çık</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.cpp" line="196"/>
+        <source>Low battery</source>
+        <translation>Düşük pil</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.cpp" line="196"/>
+        <source>%1 has %2% battery left.</source>
+        <translation>%1 'in %2% pili kaldı.</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.cpp" line="259"/>
+        <source>%1: Charging</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.cpp" line="268"/>
+        <source>No headset connected</source>
+        <translation>Herhangi bir kulaklık bağlanmadı</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.cpp" line="283"/>
+        <source>No Device Found</source>
+        <translation>Herhangi bir cihaz bulunamadı</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.cpp" line="328"/>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.cpp" line="402"/>
+        <source>Hide</source>
+        <translation>Gizle</translation>
+    </message>
+    <message>
+        <location filename="../../HeadsetControlQt/HeadsetControlQt.cpp" line="346"/>
+        <source>The application is still running in the background.</source>
+        <translation>Uygulama arka planda çalışmaya devam ediyor.</translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <location filename="../qml/Main.qml" line="23"/>
+        <source>No supported headset found.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="58"/>
+        <source>Disable lights</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="73"/>
+        <source>Send notification</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="89"/>
+        <source>Beep</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="101"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="136"/>
+        <source>Battery</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="154"/>
+        <source>Sidetone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="178"/>
+        <source>Lights</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="194"/>
+        <source>Low battery threshold</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="210"/>
+        <source>Actions on low battery</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="215"/>
+        <source>Configure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="224"/>
+        <source>Icon theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="229"/>
+        <location filename="../qml/Main.qml" line="247"/>
+        <source>System</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="229"/>
+        <source>Dark</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="229"/>
+        <source>Light</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="242"/>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
